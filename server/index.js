012ee4edbf05360659c97ea55331feac2e1ab5e0
Foridul35962 <foridulislam35962@gmail.com>
2026-03-14 19:59:52 +0600

    redis added

--- server/index.js
+++ server/index.js
@@ -2,14 +2,19 @@ import express from 'express'
 import dotenv from 'dotenv'
 import app from './src/app.js'
 import connectDB from './src/db/connectDB.js'
+import { startServer } from './src/db/redis.js'
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
 
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`server is running at http://localhost:${PORT}`)
+startServer().then(() => {
+    connectDB().then(() => {
+        app.listen(PORT, () => {
+            console.log(`server is running at http://localhost:${PORT}`)
+        })
+    }).catch(() => {
+        console.log('server started failed')
     })
 }).catch(() => {
-    console.log('server started failed')
+    console.log('redis started failed')
 })
